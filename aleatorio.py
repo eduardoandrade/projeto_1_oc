4ed1a022d4e95e56bd318ea7a4bac42148a7d987
GuilhermeApo <54627699+GuilhermeApo@users.noreply.github.com>
2019-10-13 22:29:11 -0300

    Update aleatorio.py

--- aleatorio.py
+++ aleatorio.py
@@ -1,22 +1,14 @@
 import functions
 
 def politica_substituicao_RANDOM(memoria_cache, num_conjuntos, posicao_memoria):
-  """Nessa politica de substituição, no momento que ocorrer um CACHE MISS,
-  será sorteado um elemento do conjunto para ser substituído pela nova posição
-  de memória.
-
-  Arguments:
-    memoria_cache {list} -- memóiria cache
-    num_conjuntos {int} -- quantidade de conjuntos
-    posicao_memoria {int} -- posição de memória que será acessada
+  """Nessa politica de substituição, cada vez que ocorre um evento de cache miss,
+  será sorteado um elemento do conjunto para ser substituído.
   """
   num_conjunto = get_num_conjunto_posicao_memoria(posicao_memoria, num_conjuntos)
 
   lista_posicoes = get_lista_posicoes_cache_conjunto(memoria_cache,num_conjunto, num_conjuntos)
 
-  # seleciona de forma aleatória uma das posições de memória
-  # que fazem parte do conjunto em particular e armazena dentro
-  # daquela posição o valor da memória principal
+  # faz a escolha aleatória de uma das posições de memória que fazem parte do conjunto
   posicao_troca_memoria = random.choice(lista_posicoes)
 
 
