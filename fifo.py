7e1fbf9a6579d3dde80133915e4a839af22d1031
GuilhermeApo <54627699+GuilhermeApo@users.noreply.github.com>
2019-10-13 22:36:01 -0300

    Update fifo.py

--- fifo.py
+++ fifo.py
@@ -1,13 +1,7 @@
 import functions
 
 def politica_substituicao_FIFO(memoria_cache, num_conjuntos, posicao_memoria):
-  """Nessa politica de substituição, o primeiro elemento que entra é o primeiro elemento que sai,
-  funciona exatamente como uma fila.
-
-  Arguments:
-    memoria_cache {list} -- memóiria cache
-    num_conjuntos {int} -- quantidade de conjuntos
-    posicao_memoria {int} -- posição de memória que será acessada
+  """Nessa politica de substituição, o primeiro elemento que entra é o primeiro elemento que sai
   """
   num_conjunto = functions.get_num_conjunto_posicao_memoria(posicao_memoria, num_conjuntos)
   posicao_substituir = contador_fifo[num_conjunto]
@@ -18,11 +12,11 @@ def politica_substituicao_FIFO(memoria_cache, num_conjuntos, posicao_memoria):
   print('Conjunto: {}'.format(num_conjunto))
   print('Lista posições: {}'.format(lista_posicoes))
   print('Posição para subistituição: {}'.format(posicao_substituir))
-
+  # realiza a subtituição do elemento mais antigo pelo mais recente
   memoria_cache[lista_posicoes[posicao_substituir]] = posicao_memoria
-
+  #atualiza o contador fifo
   contador_fifo[num_conjunto] += 1
-
+  # caso o número indicando o proximo endereço exceda o tamanho do conjunto retorne ao primero elemento
   if contador_fifo[num_conjunto] >= (len(memoria_cache)/num_conjuntos):
     contador_fifo[num_conjunto] = 0
 
