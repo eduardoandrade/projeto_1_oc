768fbc4aa0f53d5d0917109bea97ce907ef707c3
GuilhermeApo <54627699+GuilhermeApo@users.noreply.github.com>
2019-10-13 23:05:20 -0300

    Update mapeamento_associativo.py

--- mapeamento_associativo.py
+++ mapeamento_associativo.py
@@ -1,14 +1,7 @@
 import mapeamento_associativo_conjunto as mac
 
 def executar_mapeamento_associativo(tam_cache, posicoes_acesso_memoria, politica_substituicao):
-  """O mapeamento associativo é um tipo de mapeamento associativo por conjunto
-  ou o número de conjunto é igual a 1
-
-  Arguments:
-    tam_cache {int} -- tamanho total de palavras da cache
-    posicoes_acesso_memoria {list} - quais são as posições de memória que devem ser acessadas
-    politica_substituicao {str} -- qual será a política de subistituição
+  """O mapeamento associativo e o caso do mapeamento associativo por conjunto
+  onde o número de conjunto é igual a 1
   """
-  # o número 1 indica que haverá apenas um único conjunto no modo associativo por conjunto
-  # que é igual ao modo associativo padrão! :) SHAZAM
   mac.executar_mapeamento_associativo_conjunto(tam_cache, 1, posicoes_acesso_memoria, politica_substituicao)
